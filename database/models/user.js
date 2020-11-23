a0d574c56c8ce7580cda2b28f281ddca831b314c
Shiftyhead2 <shiftyhead2@gmail.com>
2020-11-23 20:43:18 +0100

    Added a way to delete,edit and show users

--- database/models/user.js
+++ database/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Post = require('./post');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -6,5 +7,17 @@ const UserSchema = new mongoose.Schema({
     required: true
   }
 });
+
+UserSchema.pre('remove', function(next){
+  Post.find({user: this.id},(err,posts) =>{
+    if(err){
+      next(err);
+    }else if(posts.length > 0){
+      next(new Error('This user has posts still'));
+    }else{
+      next();
+    }
+  })
+})
 
 module.exports = mongoose.model('User',UserSchema);
--- database/routes/authors.js
+++ database/routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
+const Post = require('../models/post');
 
 
 //All users route
@@ -23,6 +24,7 @@ router.get('/new',(req,res) => {
   res.render('users/new' , {user: new User()});
 })
 
+
 //Create user route
 router.post('/', async (req,res) =>{
   const user = new User({
@@ -30,12 +32,69 @@ router.post('/', async (req,res) =>{
   })
   try{
     const newUser = await user.save();
-    res.redirect('users');
+    res.redirect(`users/${newUser.id}`);
   }catch{
     res.render('users/new' , {
       user: user,
       errorMessage: 'Error creating user'
     });
+  }
+})
+
+router.get('/:id',async(req,res) => {
+  try{
+    const user = await User.findById(req.params.id);
+    const posts = await Post.find({user:user.id}).limit(6).exec();
+    res.render('users/show',{
+      user:user,
+      postsByUser:posts
+    })
+  }catch{
+    res.redirect('/');
+  }
+});
+
+
+router.get('/:id/edit', async(req,res) => {
+  try{
+    const user = await User.findById(req.params.id);
+    res.render('users/edit' , {user: user});
+  }catch{
+    res.redirect('/users');
+  }
+})
+
+router.put('/:id', async(req,res) => {
+  let user
+  try{
+    user = await User.findById(req.params.id);
+    user.name = req.body.name;
+    await user.save();
+    res.redirect(`/users/${user.id}`);
+  }catch{
+    if(user == null){
+      res.redirect('/')
+    }else{
+      res.render('users/edit' , {
+        user: user,
+        errorMessage: 'Error updated the user'
+      });
+    }
+  }
+})
+
+router.delete('/:id',async(req,res) => {
+  let user
+  try{
+    user = await User.findById(req.params.id);
+    await user.remove();
+    res.redirect("/users");
+  }catch{
+    if(user == null){
+      res.redirect('/')
+    }else{
+      res.redirect(`/users/${user.id}`)
+    }
   }
 })
 
